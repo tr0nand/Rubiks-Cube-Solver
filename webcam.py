6ac0dcea5678b021ac8380cbd5d99bcbea8be44a
123tarunanand <anandtarun2@gmail.com>
2018-09-10 15:20:14 +0530

    Average color converted into class

--- webcam.py
+++ webcam.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+from average import average_color
 
 directory = 'img'
 if not os.path.isdir(directory):
@@ -20,14 +21,26 @@ while rval and img_counter !=6:
     cv2.imshow("preview", frame)
     rval, frame = vc.read()
     key = cv2.waitKey(20)
-    if key == 27: # exit on ESC
-        break
-    elif key == 32:
+    if key == 32:
         imgname = "img"+str(img_counter)+".png"
         frame = frame[ypos:ypos + 3*size,xpos:xpos+3*size]
         cv2.imwrite(os.path.join(directory,imgname), frame)
         img_counter = img_counter + 1
         print("Face {} taken".format(img_counter))
+        cv2.imshow('Side',frame)
+        cv2.waitKey(0)
+        cubeFace = average_color(frame)
+        cubeFace.face1()
+        cubeFace.face2()
+        cubeFace.face3()
+        cubeFace.face4()
+        cubeFace.face5()
+        cubeFace.face6()
+        cubeFace.face7()
+        cubeFace.face8()
+        cubeFace.face9()
+
+
     else:
         cv2.rectangle(img=frame, pt1=(xpos, ypos),pt2=(xpos+size,ypos+size), color=(255, 0, 0), thickness=2, lineType=8, shift=0)
         cv2.rectangle(img=frame, pt1=(xpos + size, ypos),pt2=(xpos + 2*size,ypos+size), color=(255, 0, 0), thickness=2, lineType=8, shift=0)
